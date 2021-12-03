39ae06a8685aa76b494faaebe9f9c6d6bc80d0fc
theo <theo@zibell.se>
2021-12-03 16:54:11 +0100

    första steget mot hitboxes och fixa png

--- 2dspel/Program.cs
+++ 2dspel/Program.cs
@@ -8,14 +8,16 @@ float[] witchpos = new float[2];
 
 while (!Raylib.WindowShouldClose())
 {
-    Texture2D witchTexture = Raylib.LoadTexture(@"witch.png");
+    //skapa
+    Texture2D witchTexture = Raylib.LoadTexture(@"witch3.png");
     Rectangle r1 = new Rectangle(witchpos[0], witchpos[1], witchTexture.width, witchTexture.height);
-
+    Rectangle r2 = new Rectangle(1000,1000,100,100);
     //ritar saker
     Raylib.BeginDrawing();
 
     Raylib.ClearBackground(Color.WHITE);
     Raylib.DrawRectangleRec(r1, Color.SKYBLUE);
+    Raylib.DrawRectangleRec(r2, Color.RED);
 
     //konventerar från float till int (texturer behöver ints)
     int x = (int)witchpos[0];
@@ -25,7 +27,10 @@ while (!Raylib.WindowShouldClose())
 
     Raylib.EndDrawing();
 
-    // logisk
+    // rörelse
     Toolbox.Poswitch(witchpos);
+    
+    //Collison 
+    Toolbox.Hitboxes(r1,r2);
 
 }
