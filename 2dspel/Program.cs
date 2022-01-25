b9524d530a4fabe194c5b874c7894a0c5a0af4c8
Theo zibell <theo@zibell.se>
2022-01-25 13:26:47 +0100

    komenterade

--- 2dspel/Program.cs
+++ 2dspel/Program.cs
@@ -1,32 +1,37 @@
-﻿using System;
+﻿//setings till fönsteret 
+using System;
 using Raylib_cs;
 using System.Numerics;
-
 Raylib.InitWindow(Raylib.GetScreenWidth(), Raylib.GetScreenHeight(), "Hello World");
 Raylib.SetTargetFPS(60);
 
+
+//witch varjablar
 float[] witchpos = new float[2];
 Texture2D witchTexture = Raylib.LoadTexture(@"witch3.png");
 witchTexture.height = 200;
 witchTexture.width = 200;
 
 Traking T1 = new Traking();
+//bestämer att en av t1 varjablar är lika med en annan recktangel
 Rectangle witchRect1 = new Rectangle(1000, 1000, witchTexture.width, witchTexture.height);
 T1.witchRect = witchRect1;
 
-Rectangle border = new Rectangle(0, 0, Raylib.GetScreenWidth(), Raylib.GetScreenHeight());
 Rectangle r2 = new Rectangle(Raylib.GetScreenWidth() / 2, Raylib.GetScreenHeight() / 2, 100, 100);
 Rectangle r3 = new Rectangle(0, 0, Raylib.GetScreenWidth(), Raylib.GetScreenHeight());
 Rectangle[] points = new Rectangle[5];
 Rectangle[] obstical = new Rectangle[5];
-System.Console.WriteLine(border.width);
 
+//räknar på hur stor kartar ska vara 
+Rectangle border = new Rectangle(0, 0, Raylib.GetScreenWidth(), Raylib.GetScreenHeight());
 double floor = Math.Floor(border.width / 100.0) * 100.0;
 double floor1 = Math.Floor(border.height / 100.0) * 100.0;
 int[] mapSize = new int[2];
+//glömt vad den gör 
 mapSize[0] = Convert.ToInt32(floor);
 mapSize[1] = Convert.ToInt32(floor1);
 
+//ger värden på flera obijekt 
 for(int i = 0; i < 5; i++){
     Rectangle obsticalRefrens = new Rectangle(Raylib.GetScreenWidth() / 4, Raylib.GetScreenHeight() / 2 + i * 100, 100, 100);
     obstical[i] = obsticalRefrens;
